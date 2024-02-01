825e9dfc97d1630bf581fc6c5491d65e0bc9ed94
Umang Patel <patelumang858@gmail.coml>
2024-01-31 22:25:34 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Index from './pages/Index';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import PageNotFound from './pages/PageNotFound';
+
+export const links = {
+  indexURL: '/index',
+  login: '/login',
+  signUp: '/signup',
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path={links.indexURL} element={<Index />} />
+        <Route path={links.login} element={<Login />} />
+        <Route path={links.signUp} element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
